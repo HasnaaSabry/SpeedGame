f5eaffac1dd0d763625f05d039a9f1800a66ee73
HasnaaMohammed <hasnaamohammed1993@gmail.com>
2017-12-22 13:16:05 +0200

    Add files via upload

---
+++ speedgame.js
@@ -0,0 +1,34 @@
+
+var num=0;
+var key=11;
+var count=0;
+var all=0;
+    function start(){
+        count=0;
+        all=0;
+        document.getElementById('score').innerHTML=count;
+        document.getElementById('final1').innerHTML="";
+    document.getElementById('final2').innerHTML="";
+   myint=setInterval(inter, 1000);     
+        
+    }
+function inter(){
+    num=Math.floor(Math.random()*10);
+    document.getElementById('box').innerHTML=num;
+    all++;
+}
+function press(){
+    key=event.keyCode-48;
+    if(num==key){
+        count++;     
+    }
+    document.getElementById('score').innerHTML=count;
+}
+ function stop(){
+    clearInterval(myint);
+    document.getElementById('final1').innerHTML="Your Final Score:";
+    document.getElementById('final2').innerHTML=count+"/"+all;
+    
+    
+}  
+        document.addEventListener("keypress", press);
